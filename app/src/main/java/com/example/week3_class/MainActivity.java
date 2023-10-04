a60c84230c7c18f043bb743ff6985f69caf09e0b
HTNam1008 <skyisthelimit.1008@gmail.com>
2023-10-04 23:09:54 +0700

    first commit

---
+++ app/src/main/java/com/example/week3_class/MainActivity.java
@@ -0,0 +1,153 @@
+package com.example.week3_class;
+
+import android.app.Activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Context context;
+    private int duration=Toast.LENGTH_SHORT;
+    private Button btnReset ;
+    private Button btnSignUp ;
+
+    private EditText edtUsername;
+    private EditText edtPassword;
+    private EditText edtRetype;
+    private EditText edtBirthday;
+    private RadioGroup rgGender;
+    private CheckBox checkTennis;
+    private CheckBox checkOthers;
+    private CheckBox  checkFootball;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        btnReset=findViewById(R.id.btnReset);
+        btnSignUp=findViewById(R.id.btnSignUp);
+
+        edtUsername=findViewById(R.id.username);
+        edtPassword=findViewById(R.id.password);
+        edtRetype=findViewById(R.id.retype);
+        edtBirthday=findViewById(R.id.birthday);
+        rgGender=findViewById(R.id.gender);
+        checkTennis=findViewById(R.id.tennis);
+        checkFootball=findViewById(R.id.football);
+        checkOthers=findViewById(R.id.others);
+
+        btnReset.setOnClickListener(this);
+        btnSignUp.setOnClickListener(this);
+    }
+
+
+
+
+    private boolean isRetypeCorrect() {
+        boolean check=true;
+        if (!edtPassword.getText().toString().equals(edtRetype.getText().toString())) {
+            check=false;
+        }
+        return check;
+    }
+
+    private boolean isFullInfo() {
+        boolean check=true;
+        if (edtUsername.getText().toString().isEmpty()) {
+            check = false;
+        }
+        if (edtPassword.getText().toString().isEmpty()) {
+            check=false;
+        }
+        if (edtRetype.getText().toString().isEmpty()){
+            check=false;
+        }
+
+        if (rgGender.getCheckedRadioButtonId()==-1) {
+            check=false;
+        }
+        if (!checkFootball.isChecked() && !checkTennis.isChecked() &&!checkOthers.isChecked()){
+            check=false;
+        }
+        return check;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == btnReset.getId()) {
+            edtUsername.setText("");
+            edtPassword.setText("");
+            edtRetype.setText("");
+
+            rgGender.clearCheck();
+            checkTennis.setChecked(false);
+            checkTennis.setSelected(false);
+
+            checkFootball.setChecked(false);
+            checkFootball.setSelected(false);
+
+            checkOthers.setChecked(false);
+            checkOthers.setSelected(false);
+        }
+
+        if (v.getId() == btnSignUp.getId()) {
+            if (isFullInfo()) {
+                if (isRetypeCorrect()) {
+                    // Tạo một Bundle và đặt các giá trị vào đó
+                    Bundle bundle = new Bundle();
+                    bundle.putString("username", edtUsername.getText().toString());
+                    bundle.putString("password", edtPassword.getText().toString());
+                    bundle.putString("birthday", edtBirthday.getText().toString());
+                    RadioButton gender = findViewById(rgGender.getCheckedRadioButtonId());
+                    bundle.putString("gender", gender.getText().toString());
+
+                    bundle.putString("hobbies", checkHobbies());
+
+// Tạo Intent để mở Activity B và đính kèm Bundle
+                    Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                    intent.putExtras(bundle);
+
+// Khởi động Activity B
+                    startActivity(intent);
+                }
+                else {
+                    context=getApplicationContext();
+                    Toast.makeText(context,"Nhập lại mật khẩu sai",duration).show();
+                }
+            }
+            else {
+                context=getApplicationContext();
+                Toast.makeText(context,"Hãy nhập đủ thông tin",duration).show();
+            }
+        }
+    }
+
+
+    public String checkHobbies() {
+        String hobbies="";
+        if (checkFootball.isChecked()) {
+            hobbies=hobbies+ checkFootball.getText().toString();
+        }
+        if (checkTennis.isChecked()) {
+            hobbies=hobbies+", "+checkTennis.getText().toString();
+        }
+        if (checkOthers.isChecked()) {
+            hobbies=hobbies+", "+checkOthers.getText().toString();
+        }
+        return hobbies;
+    }
+}
---
+++ app/src/main/java/com/example/week3_class/SignUpActivity.java
@@ -0,0 +1,66 @@
+package com.example.week3_class;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import java.util.ConcurrentModificationException;
+
+public class SignUpActivity extends AppCompatActivity {
+    private Context context;
+    private int duration=Toast.LENGTH_SHORT;
+
+    private Button btnExit;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singup);
+
+        btnExit=findViewById(R.id.btnExit);
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        Bundle receivedBundle = getIntent().getExtras();
+        if (receivedBundle != null) {
+            String username = receivedBundle.getString("username");
+            String password = receivedBundle.getString("password");
+
+            String birthday = receivedBundle.getString("birthday");
+            String gender = receivedBundle.getString("gender");
+            String hobbies = receivedBundle.getString("hobbies");
+
+
+            // Bây giờ bạn có thể sử dụng username và password trong Activity B
+            // Ví dụ: hiển thị chúng trên giao diện người dùng
+            TextView txtUsername = findViewById(R.id.txtUsername);
+            TextView txtPassword = findViewById(R.id.txtPassword);
+            TextView txtBirthday = findViewById(R.id.txtBirthday);
+            TextView txtGender = findViewById(R.id.txtGender);
+            TextView txtHobbies = findViewById(R.id.txtHobbies);
+
+            txtUsername.setText("Username: " + username);
+            txtPassword.setText(password);
+            txtBirthday.setText("Birthday: "+birthday);
+            txtGender.setText("Gender: "+gender);
+            txtHobbies.setText("Hobbies: "+hobbies);
+
+        }
+
+    }
+
+
+}
